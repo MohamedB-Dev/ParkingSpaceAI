345b684b591486f1c9b8dd7d0ba669dcddb97150
Mohamed Adnan <mohamed.b4043@gmail.com>
2023-12-13 23:21:48 +0300

    Refactoring

--- run_video_analysis.py
+++ run_video_analysis.py
@@ -4,75 +4,77 @@ import pickle
 import pandas as pd
 from ultralytics import YOLO
 import cvzone
+import json
 
+# Define file paths and frame dimensions
+pickle_file = 'parking_data.pkl'
+class_file = 'coco.txt'
+yolo_weights = 'yolov8s.pt'
+video_file = 'video.mp4'
+frame_width = 1020
+frame_height = 500
+frame_process_interval = 3  # Process every third frame
 
-with open("pickle","rb") as f :
-            data=pickle.load(f)
-            polylines,parking_number=data['polylines'],data['parking_number']
+# Load parking space data
+with open(pickle_file, "rb") as f:
+    data = pickle.load(f)
+    polylines, parking_numbers = data['polylines'], data['parking_numbers']
 
+# Load class labels
+with open(class_file, "r") as file:
+    class_list = file.read().strip().split("\n")
 
-my_file = open("coco.txt", "r")
-data = my_file.read()
-class_list = data.split("\n")
+# Initialize YOLO model
+model = YOLO(yolo_weights)
 
-model=YOLO('yolov8s.pt')
+# Start video capture
+cap = cv2.VideoCapture(video_file)
 
+def process_frame(frame, polylines, parking_numbers):
+    """Process a single frame for parking spot detection."""
+    frame = cv2.resize(frame, (frame_width, frame_height))
+    results = model.predict(frame)
+    detections = pd.DataFrame(results[0].boxes.data).astype("float")
 
-cap=cv2.VideoCapture('video.mp4')
+    detected_cars = []
+    for index, row in detections.iterrows():
+        x1, y1, x2, y2, _, class_id = row.astype(int)
+        if class_list[class_id] == 'car':
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+            detected_cars.append((cx, cy))
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
 
-count=0
+    for i, polyline in enumerate(polylines):
+        cv2.polylines(frame, [polyline], True, (0, 255, 0), 2)
+        cvzone.putTextRect(frame, str(parking_numbers[i]), tuple(polyline[0]), 1, 1)
+        # Check if cars are in the parking spots
+        for car in detected_cars:
+            if cv2.pointPolygonTest(polyline, car, False) >= 0:
+                cv2.circle(frame, car, 5, (255, 0, 0), -1)
+                cv2.polylines(frame, [polyline], True, (0, 0, 255), 2)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        continue
-   
-    count += 1
-    if count % 3 != 0:
-       continue
+    # Display the frame
+    cv2.imshow('Frame', frame)
 
-    frame=cv2.resize(frame,(1020,500))
-    frame_copy = frame.copy()
-    results=model.predict(frame)
-    a=results[0].boxes.data
-    px=pd.DataFrame(a).astype("float")
-    list1=[]    
-    list3=[]
-    for index,row in px.iterrows():
- 
-        x1=int(row[0])
-        y1=int(row[1])
-        x2=int(row[2])
-        y2=int(row[3])
-        d=int(row[5])
-        
-        c=class_list[d]
-        cx=int(x1+x2)//2
-        cy=int(y1+y2)//2
-        if 'car' in c:
-            list1.append([cx,cy])
-            cv2.rectangle(frame,(x1,y1),(x2,y2),(255,255,255),2) 
-        if 'person' in c:
-            list3.append([cx,cy])
-            cv2.rectangle(frame,(x1,y1),(x2,y2),(139,139,0),2) 
+def main():
+    """Main function to run the video analysis."""
+    frame_counter = 0
 
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            continue
 
-    counter1=[]
-    list2=[]
-    for i,polyline in enumerate (polylines):
-        list2.append(i)
-        cv2.polylines(frame,[polyline],True,(0,255,0),2)
-        cvzone.putTextRect(frame,f'{parking_number[i]}', tuple(polyline[0]),1,1)  
-        for i1 in list1 :
-            cx1=i1[0]
-            cy1=i1[1]
-            result=cv2.pointPolygonTest(polyline,((cx1,cy1)),False)
-            if result>=0:
-                cv2.circle(frame,(cx1,cy1),5,(255,0,0),-1) 
-                cv2.polylines(frame,[polyline],True,(0,0,255),2)
-                counter1.append(cx1) 
+        frame_counter += 1
+        if frame_counter % frame_process_interval == 0:
+            process_frame(frame, polylines, parking_numbers)
 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    cv2.imshow('FRAME', frame)  
-    key = cv2.waitKey(1) & 0xFF
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
